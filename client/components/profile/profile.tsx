0c5ba99351ca2931738fe06d833a0812cad8fc09
hlimouni <limouni.hossam@gmail.com>
2022-08-27 21:11:28 +0100

    adding components for profile

--- client/components/profile/profile.tsx
+++ client/components/profile/profile.tsx
@@ -1,13 +1,12 @@
 import React, { useContext } from 'react'
 import { Button, Typography } from '@mui/material'
 import {AppContext} from '../../context/AppContext'
-
+import Cover from './cover'
 export default function Profile() {
   const {state} = useContext(AppContext)
   return (
     <div>
-        <Typography variant='h1'>profile page</Typography>
-        <Button variant='contained'>Press me</Button>
+        <Cover/>
         <p>{state.mainUser.id}</p>
         <p>{state.mainUser.userName}</p>
     </div>
