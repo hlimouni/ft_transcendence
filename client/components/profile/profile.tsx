6d0afa1a79dd997b90e693b9d5143792cc157e57
Hossam Limouni <limouni.hossam@gmail.com>
2022-08-28 21:28:50 +0100

    adding useEffect hook to properly fetch friends

--- client/components/profile/profile.tsx
+++ client/components/profile/profile.tsx
@@ -1,14 +1,23 @@
-import React, { useContext } from 'react'
-import { Button, Typography } from '@mui/material'
+import { stat } from 'fs'
+import React, { useContext, useState, useEffect } from 'react'
 import {AppContext} from '../../context/AppContext'
 import Cover from './cover'
+import MenuBar from './menubar'
+import { fetchFriends } from './utils'
 export default function Profile() {
-  const {state} = useContext(AppContext)
+  const {state} = useContext(AppContext);
+  const cntx = useContext(AppContext);
+
+	useEffect(() => {
+		console.log("state mainUser : effec ", state.mainUser);
+		if (state.mainUser) {
+			fetchFriends(cntx);
+		}
+	}, [state.mainUser]);
   return (
     <div>
         <Cover/>
-        <p>{state.mainUser.id}</p>
-        <p>{state.mainUser.userName}</p>
+        <MenuBar/>
     </div>
   )
 }
