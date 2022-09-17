08207484cb8ad375d09a63f5f3c5901f095c5077
hlimouni <limouni.hossam@gmail.com>
2022-09-17 22:49:26 +0100

    edit profile front working

--- client/components/profile/cover.tsx
+++ client/components/profile/cover.tsx
@@ -5,26 +5,49 @@ import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
 import MenuBar from './menubar'
 import EditIcon from '@mui/icons-material/Edit';
-import { Typography } from '@mui/material'
+import EditDialog from './editDialog'
 
-type Props = {}
-
-const Cover = (props: Props) => {
+const Cover = () => {
   const {state} = useContext(AppContext);
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className={ProfileStyle.cover}>
        <img className={ProfileStyle.coverImage} src='BACKGROUND@4x.png' alt='cover image'/>
        <div className={ProfileStyle.profileBanner}>
-          {/* <img src={state.mainUser.image} alt="User avatar"/> */}
           <div className={ProfileStyle.profileInfos}>
-            <Avatar alt={state.mainUser.userName} src={state.mainUser.image} sx={{width: '120px', height: '120px'}}/>
+            <Avatar
+              alt={state.mainUser.userName}
+              src={state.mainUser.image}
+              sx={{width: '120px', height: '120px'}}
+            />
             <div className={ProfileStyle.profileText}>
               <h1>{state.mainUser.firstName} {state.mainUser.lastName} <span className={ProfileStyle.sub}>({state.mainUser.userName})</span></h1>
               <h6>{[...state.friends].length} friends</h6>
             </div>
           </div>
-          <Button className={ProfileStyle.editButton} startIcon={<EditIcon/>} variant='contained' sx={{letterSpacing: '.12em', whiteSpace: 'nowrap', margin: '2.2rem', padding: '2em 3em'}}>Edit Profile</Button>
+          <Button
+            onClick={handleClickOpen}
+            className={ProfileStyle.editButton}
+            startIcon={<EditIcon/>}
+            variant='contained'
+            sx={{
+              letterSpacing: '.12em',
+              whiteSpace: 'nowrap',
+              margin: '2.2rem',
+              padding: '2em 3em'
+              }}>
+                Edit Profile
+          </Button>
+          <EditDialog openState={open} closeHandler={handleClose}/>
        </div>
     </div>
   )
