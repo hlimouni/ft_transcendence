a0274802226b4c060d2d701a349d928f93c3f3e5
hlimouni <limouni.hossam@gmail.com>
2022-09-25 00:15:11 +0100

    2fa

---
+++ client/components/profile/friends.tsx
@@ -0,0 +1,160 @@
+import axios from 'axios'
+import React, { useContext, useEffect, useState } from 'react'
+import Loading from '../../components/Login/Loading'
+import UserCard from '../../components/Users/UserCard'
+import { AppContext } from '../../context/AppContext'
+import { User } from '../../utils/interfaces'
+import { Container } from '@mui/material'
+import { TIMEOUT } from 'dns'
+
+const Friends = () => {
+  const { state, setUsers, setMainUser, setFriends } = useContext(AppContext)
+  const [friendsId, setfriendsId] = useState<any[]>([])
+  const [pendingRequestsIds, setPendingRequestsIds] = useState<any[]>([])
+  const [blockedUsersIds, setblockedUsersIds] = useState<any[]>([])
+  const [RecievedRequests, setRecievedRequests] = useState<any[]>([])
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.SERVER_HOST}/users/me`, { withCredentials: true })
+      .then((res) => {
+        setMainUser({ ...res.data })
+      })
+      .catch(() => {})
+  }, [])
+
+  useEffect(() => {
+    if (state.mainUser) {
+      fetchUsers()
+      fetchpendingRequests()
+      fetchFriends()
+      fetchblockedUsersIds()
+      fetchRecievedRequests()
+      state.eventsSocket.on("A_USER_STATUS_UPDATED", (user: any) => {
+        fetchUsers();
+        fetchFriends();
+        fetchpendingRequests()
+        fetchblockedUsersIds()
+        fetchRecievedRequests()
+        console.log("user online data");   
+      });
+      state.eventsSocket.on("UPDATE_DATA", () => {
+        fetchUsers();
+        fetchFriends();
+        fetchpendingRequests()
+        fetchblockedUsersIds()
+        fetchRecievedRequests()
+        console.log("update data");        
+      });
+  
+    }
+  }, [state.mainUser])
+
+  async function fetchUsers() {
+    try {
+        const res = await axios
+          .get(`${process.env.SERVER_HOST}/users`, { withCredentials: true })
+            if (res.data.length){
+                setUsers(
+                  res.data.filter((user: User) => {
+                    return user.id != state.mainUser.id
+                  }),
+                )
+            }else{
+              setUsers([])
+            }
+    } catch (error) {
+      console.log('error!!!')
+    }
+  }
+
+  async function fetchFriends() {
+    try {
+      const res = await axios.get(`${process.env.SERVER_HOST}/users/id/${state.mainUser.id}/friends`, {withCredentials: true})
+      
+        setFriends([...res.data]);
+        setfriendsId([...res.data].map((e) => e.id))
+        console.log("friends fetched");
+    } catch (error) {
+      console.log('error!!!')
+    }
+  }
+
+  async function fetchRecievedRequests() {
+    try {
+      const res = await axios.get(`${process.env.SERVER_HOST}/users/recievedrequests`, {withCredentials: true},)
+
+      setRecievedRequests([...res.data].map((e) => e.id));
+      console.log("recievedrequets fetched",RecievedRequests);
+    } catch (error) {
+      console.log('error');
+    }
+  }
+
+  // fetchpendingIds();
+  async function fetchpendingRequests() {
+    try {
+      const res = await axios.get(
+        `${process.env.SERVER_HOST}/users/sentrequests`,
+        { withCredentials: true },
+      )
+      if (res.data.length) {
+        setPendingRequestsIds([...res.data].map((e) => e.id))
+      } else {
+        setPendingRequestsIds([])
+        console.log('no friend request')
+      }
+    } catch (error) {
+      console.log('error!!!')
+    }
+  }
+
+  async function fetchblockedUsersIds() {
+    try {
+      const res = await axios.get(`${process.env.SERVER_HOST}/users/blocked`, {withCredentials: true})
+      if(res.data.length){
+        setblockedUsersIds([...res.data].map((e) => e.id))
+        console.log("blocked users fetched successfuly")
+      } else {
+        setblockedUsersIds([])
+        console.log("no blocked users!!")
+      }
+    } catch (error) {
+      
+    }
+  }
+  // useEffect(()=>{
+  //     fetchpendingIds();
+  // },[pendingIds])
+
+  return (
+    <>
+    {/* <Container style={{ color: 'white', padding: '0px' }}> */}
+      {!state.friends ? (
+        <Loading />
+      ) : (
+        <div className='usersDiv'>
+          {/* <h1>
+            Users
+          </h1> */}
+          <div className="usersvl">Friends</div>
+          
+            {state.friends.length == 0 ?
+            (<div className='emptyusers'>no Friends found.</div>) : 
+            (
+              <div className="condiv">
+                {state.friends.map((user: any) => {
+                  return (
+                    <UserCard key={user.id} {...user} pendingRequestsIds={pendingRequestsIds} friendsId={friendsId} blockedUsersIds={blockedUsersIds} RecievedRequests={RecievedRequests} />
+                  )
+                })}
+              </div>
+            )}
+        </div>
+      )}
+    {/* </Container> */}
+    </>
+  )
+}
+
+export default Friends
